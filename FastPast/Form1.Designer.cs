09ac7ce62cc000547a2c8340f5225e938a015934
Rémi Giacometti <remi.giacometti@gmail.com>
2024-10-02 21:45:19 +0200

    Form Disgne

--- FastPast/Form1.Designer.cs
+++ FastPast/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FastPast
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            label1 = new Label();
+            hideButton = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(23, 45);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(287, 23);
+            textBox1.TabIndex = 0;
+            textBox1.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Eras Medium ITC", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(23, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 24);
+            label1.TabIndex = 1;
+            label1.Text = "FastPast";
+            // 
+            // hideButton
+            // 
+            hideButton.Location = new Point(23, 87);
+            hideButton.Name = "hideButton";
+            hideButton.Size = new Size(114, 23);
+            hideButton.TabIndex = 2;
+            hideButton.Text = "Hide";
+            hideButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(342, 139);
+            Controls.Add(hideButton);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "MainForm";
+            Text = "Form ";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button hideButton;
     }
 }
